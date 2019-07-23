67443cb4a87d93c260b7ee9fa06773de0722e7b5
jerry <jm90_yang@163.com>
2019-07-23 14:50:46 +0800

    使用POI完成文件的读取

--- hero-web/src/main/java/com/house/hero/easyPoi/excelImport/ExcelImportDemo.java
+++ hero-web/src/main/java/com/house/hero/easyPoi/excelImport/ExcelImportDemo.java
@@ -3,10 +3,20 @@ package com.house.hero.easyPoi.excelImport;
 import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ImportParams;
 import com.house.hero.common.bean.Employee;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +27,12 @@ import java.util.List;
 @RequestMapping("/import")
 public class ExcelImportDemo {
 
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExcelImportDemo.class);
+    /**
+     * easyPoi 超简单导入
+     * @param file
+     */
     @RequestMapping("/excel")
     public void importExcel(MultipartFile file) {
 
@@ -34,4 +50,68 @@ public class ExcelImportDemo {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Poi导入
+     * @param file
+     */
+    @RequestMapping("/poi/importExcel")
+    public void poiImportExcel(MultipartFile file) throws IOException {
+
+        List<Employee> employeeList = getObjInputStream(file.getInputStream(), file.getOriginalFilename());
+        System.out.println(employeeList);
+    }
+    /**
+     * 使用POI完成文件的读取
+     * @param inputStream
+     * @param originalFilename
+     * @return
+     */
+    private List<Employee> getObjInputStream(InputStream inputStream, String originalFilename) {
+
+        List<Employee> resultDto = new ArrayList<>();
+
+        Workbook workbook = null;
+        try {
+
+            try {
+                if (originalFilename.matches("^.+\\.(?i)(xls)$")) {
+                    workbook = new HSSFWorkbook(inputStream);
+                } else if (originalFilename.matches("^.+\\.(?i)(xlsx)$")) {
+                    workbook = WorkbookFactory.create(inputStream);
+                } else {
+                    System.out.println("文件格式错误");
+                }
+            } catch (IOException e) {
+                LOGGER.error("order.import.error,原因:{}", e);
+            }
+
+                Employee employee = new Employee();
+                Sheet sheet = workbook.getSheetAt(0);
+                //最后一行的行号
+                int lastRowNum = sheet.getLastRowNum();
+            for (int rowNm = 1; rowNm <= lastRowNum; rowNm++) {
+
+                //获取第某行的数据(从rowNm开始)
+                Row row = sheet.getRow(rowNm);
+
+                //第一行第一列数据
+                double firstStringCellValue = row.getCell(0).getNumericCellValue();
+                employee.setAge((int) firstStringCellValue);
+                //第一行第二列数据
+                String secondStringCellValue = row.getCell(1).getStringCellValue();
+                employee.setName(secondStringCellValue);
+                //第一行第三列数据
+                double numericCellValue = row.getCell(2).getNumericCellValue();
+                employee.setSalary(numericCellValue);
+
+                //封装对象集合
+                resultDto.add(employee);
+            }
+        } catch (Exception e) {
+            LOGGER.error("导入失败,原因:{}", e);
+        }
+        return resultDto;
+    }
+
 }
