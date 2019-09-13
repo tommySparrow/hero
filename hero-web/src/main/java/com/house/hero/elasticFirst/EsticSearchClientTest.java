9f65aa510271cd0365796cafd7263f0071ff8245
jm90_yang <jm90_yang@163.com>
2019-09-14 07:50:40 +0800

    创建elasticsearch索引库

---
+++ hero-web/src/main/java/com/house/hero/elasticFirst/EsticSearchClientTest.java
@@ -0,0 +1,34 @@
+package com.house.hero.elasticFirst;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+
+/**
+ * elasticsearch
+ * @author jerry
+ */
+public class EsticSearchClientTest {
+
+    public static void main(String[] args) throws Exception {
+
+        //1.创建一个settings
+        Settings settings = Settings.builder()
+                .put("cluster.name","my-elasticsearch")
+                .build();
+        //2.创建一个客户端client对象
+        TransportClient client = new PreBuiltTransportClient(settings);
+        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"),9301));
+        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"),9302));
+        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"),9303));
+        //使用client创建索引库
+        client.admin().indices().prepareCreate("index_hello")
+                //执行方法
+                .get();
+        //关闭连接
+        client.close();
+    }
+}
